f69651e161203d214793d67de3a0b6b3195a431e
josyf625 <65207410+josyf625@users.noreply.github.com>
2022-02-20 20:47:23 +0900

    weather completed
    All the informations are added.
    time to use css and make it prettier

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+const API_KEY = "51c65cbea3abb6c7ce0a1fc8afa42539"
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+
+function onGeoOk(loc){
+    const lat = loc.coords.latitude;
+    const lng = loc.coords.longitude;
+    console.log(`you are at ${lat}, ${lng}`)
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    console.log(url)
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+         });
+}
+
+function onGeoError(){
+    alert("Can't find you. no weather for you")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError)
